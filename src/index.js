4f0a5fedf36c342d1d87b5109a34ae3dbec93184
unknown <71916156+xyzxx@users.noreply.github.com>
2021-03-13 19:25:11 +0700

    delete function menambahkan fungsi delete todo

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import "./styles.css";
 const app = document.querySelector("#app");
 const todoInput = document.querySelector("input");
 const button = document.querySelector("button");
+const ul = document.querySelector("ul");
 
 button.addEventListener("click", addTodo);
+ul.addEventListener("click", deleteTodo);
 
 function addTodo(e) {
   e.preventDefault();
-  const ul = document.createElement("ul");
+
   const li = document.createElement("li");
   li.innerText = todoInput.value;
   ul.appendChild(li);
   app.appendChild(ul);
   todoInput.value = "";
+}
+
+function deleteTodo(e) {
+  e.target.nodeName === "LI" && e.target.remove();
 }
